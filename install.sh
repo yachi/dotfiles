adf58f8f9499af140db6aa24e9154025fbd408e1
Yachi Lo <yaachi@gmail.com>
2013-10-18 00:16:47 +0800

    install python and powerline too

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+brew install python
+pip show powerline > /dev/null || pip install git+https://github.com/Lokaltog/powerline
+
 test -d $HOME/github || mkdir -v $HOME/github
 test -d $HOME/github/dircolors-solarized || git clone https://github.com/seebi/dircolors-solarized.git $HOME/github/dircolors-solarized
+
 ln -svf "$(pwd)/.zshrc" $HOME/
 ln -svf "$(pwd)/.tmux.conf" $HOME/
 ln -svf "$(pwd)/.janus" $HOME/
