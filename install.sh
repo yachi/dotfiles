1757a8bdcb2d723cbc409d01b4d432d282dfa904
Yachi Lo <yaachi@gmail.com>
2013-10-18 00:11:17 +0800

    clone dircolors too

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+test -d $HOME/github || mkdir $HOME/github
+test -d $HOME/github/dircolors-solarized || git clone https://github.com/seebi/dircolors-solarized.git $HOME/github/dircolors-solarized
 ln -svf "$(pwd)/.zshrc" $HOME/
 ln -svf "$(pwd)/.tmux.conf" $HOME/
 ln -svf "$(pwd)/.janus" $HOME/
