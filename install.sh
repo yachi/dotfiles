b446e113c964f47ab06fc67eafaa6ce319cfc42f
Yachi Lo <yaachi@gmail.com>
2013-10-18 11:07:44 +0800

    init janus too

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 # prerequisite:
 #   homebrew
 #   oh my zsh + zpython
+function init_submodules(){
+  pushd $1
+  git submodule init
+  git submodule sync
+  git submodule update
+  popd
+}
 
 brew install python
 pip show powerline > /dev/null || pip install git+https://github.com/Lokaltog/powerline
@@ -10,10 +17,9 @@ pip show powerline > /dev/null || pip install git+https://github.com/Lokaltog/po
 test -d $HOME/github || mkdir -v $HOME/github
 test -d $HOME/github/dircolors-solarized || git clone https://github.com/seebi/dircolors-solarized.git $HOME/github/dircolors-solarized
 
-git submodule init
-git submodule sync
-git submodule update
-
 ln -svf "$(pwd)/.zshrc" $HOME/
 ln -svf "$(pwd)/.tmux.conf" $HOME/
 ln -svf "$(pwd)/.janus" $HOME/
+
+init_submodules .
+init_submodules $HOME/.janus
