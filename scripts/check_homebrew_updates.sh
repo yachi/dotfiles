ae9655595d07ae6b3f1af208d49ceef350ebba70
Yachi Lo <yaachi@gmail.com>
2013-10-18 12:37:00 +0800

    add homebrew update checking script

---
+++ scripts/check_homebrew_updates.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/zsh
+
+brew update > /dev/null
+outdated=$(brew outdated)
+title="Outdated homebrew packages"
+[ -z "$outdated" ] || terminal-notifier -title $title -message "$outdated"
