a71248a576477c42fae8b34c8c90a1f64dacd03a
Yachi Lo <yaachi@gmail.com>
2013-11-09 16:25:57 +0800

    add sbtconfig

--- install.sh
+++ install.sh
@@ -49,6 +49,7 @@ symlink_to_home .janus
 symlink_to_home .janus/.vimrc.after
 symlink_to_home .gemrc
 symlink_to_home .gitconfig
+symlink_to_home .sbtconfig
 symlink_to_home gitignore .gitignore
 
 log "installing powerline fonts"
