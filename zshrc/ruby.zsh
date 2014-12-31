3982cae5e3fdc0c030706ee5c3ad98d3be73ee40
Yachi Lo <yaachi@gmail.com>
2014-12-31 10:50:34 +0800

    install ruby with railsexpress

--- zshrc/ruby.zsh
+++ zshrc/ruby.zsh
@@ -6,4 +6,17 @@ export RUBY_GC_MALLOC_LIMIT=64000000
 export RUBY_GC_OLDMALLOC_LIMIT=64000000
 
 # default ruby
-chruby ruby-2.1.5
+default_ruby="2.2.0"
+chruby "ruby-$default_ruby"
+
+install-ruby() {
+  echo "$default_ruby"
+  if [ -z $1 ]; then
+    version="$default_ruby"
+  else
+    version="$1"
+  fi
+  echo "installing ruby $version..."
+  patch_name="/tmp/ruby-$version-railsexpress.patch"
+  curl -s "https://raw.githubusercontent.com/skaes/rvm-patchsets/master/patchsets/ruby/$version/railsexpress" | xargs -I {} curl -s "https://raw.githubusercontent.com/skaes/rvm-patchsets/master/patches/ruby/$version/{}" > "$patch_name" && ruby-install ruby "$version" -p "$patch_name"
+}
