4fd72d589e16ddfe0c181ad997b3784087952a4f
Yachi Lo <yaachi@gmail.com>
2017-01-03 11:25:51 +0800

    update base16

--- zshrc/base16-shell.zsh
+++ zshrc/base16-shell.zsh
@@ -1,3 +1,3 @@
 # Base16 Shell
-BASE16_SHELL="$DOTFILES/peru/base16-shell/base16-default.dark.sh"
+BASE16_SHELL="$DOTFILES/peru/base16-shell/scripts/base16-default-dark.sh"
 [[ -s "$BASE16_SHELL" ]] && source "$BASE16_SHELL"
